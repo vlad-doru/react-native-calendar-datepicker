68c11b46320bec61694ca9a3ef2d3097c63bdb3b
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-08-26 17:29:42 +0300

    There is a collision that we want to avoid.

--- demo/index.ios.js
+++ demo/index.ios.js
@@ -1,6 +1,5 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Example app using the Calendar component.
  * @flow
  */
 
@@ -11,21 +10,111 @@ import {
   Text,
   View
 } from 'react-native';
+import Calendar from 'react-native-calendar-datepicker';
+import Moment from 'moment';
+
+type State = {
+  date?: Moment,
+};
 
 class demo extends Component {
+  state: State
+
+  constructor(props: Object) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
+    const BLUE = '#2196F3';
+    const WHITE = '#FFFFFF';
+    const GREY = '#BDBDBD';
+    const BLACK = '#424242';
+    const LIGHT_GREY = '#F5F5F5';
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View>
+          <Text style={styles.dateText}>
+            {this.state.date ?
+            this.state.date.format('DD-MM-YYYY h:mm a') :
+            "No date selected"}
+          </Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{flex: 1}}></View>
+          <Calendar
+            onChange={(date) => this.setState({date})}
+            selected={this.state.date}
+            minDate={Moment().startOf('day')}
+            maxDate={Moment().add(10, 'years').startOf('day')}
+            //General Styling}
+            style={{
+              borderWidth: 1,
+              borderColor: GREY,
+              borderRadius: 5,
+              alignSelf: 'center',
+              marginTop: 20,
+            }}
+            barView={{
+              backgroundColor: BLUE,
+              padding: 10,
+            }}
+            barText={{
+              fontWeight: 'bold',
+              color: WHITE,
+            }}
+            stageView={{
+              padding: 0,
+            }}
+            // Day selector styling
+            dayHeaderView={{
+              backgroundColor: LIGHT_GREY,
+              borderBottomColor: GREY,
+            }}
+            dayHeaderText={{
+              fontWeight: 'bold',
+              color: BLACK,
+            }}
+            dayRowView={{
+              borderColor: LIGHT_GREY,
+              height: 40,
+            }}
+            dayText={{
+              color: BLACK,
+            }}
+            dayDisabledText={{
+              color: GREY,
+            }}
+            dayTodayText={{
+              fontWeight: 'bold',
+              color: BLUE,
+            }}
+            daySelectedText={{
+              fontWeight: 'bold',
+              backgroundColor: BLUE,
+              color: WHITE,
+              borderRadius: 15,
+              overflow: 'hidden',
+            }}
+            // Styling month selector.
+            monthText={{
+              color: BLACK,
+              borderColor: BLACK,
+            }}
+            monthDisabledText={{
+              color: GREY,
+              borderColor: GREY,
+            }}
+            // Styling year selector.
+            yearMinTintColor={BLUE}
+            yearMaxTintColor={GREY}
+            yearText={{
+              color: BLACK,
+            }}
+            />
+          <View style={{flex: 1}}></View>
+        </View>
       </View>
     );
   }
@@ -34,20 +123,14 @@ class demo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: 40,
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  dateText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 20,
+  }
 });
 
 AppRegistry.registerComponent('demo', () => demo);
