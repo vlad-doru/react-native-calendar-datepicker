8b00721ccb56bd688746bfbf6730232ffdad6a9e
Jason Gaare <jason.d.gaare@gmail.com>
2017-02-02 10:23:09 -0600

    updated readme and removed unnecessary padding

--- src/container/Calendar.react.js
+++ src/container/Calendar.react.js
@@ -148,7 +148,6 @@ export default class Calendar extends Component {
             { this.props.showArrows && this.state.stage === DAY_SELECTOR && previousMonthValid ?
               <TouchableHighlight
                 hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
-                style={{ padding: 4 }}
                 underlayColor={barStyle ? barStyle.backgroundColor : 'transparent'}
                 onPress={this._previousMonth}
               >
@@ -170,7 +169,6 @@ export default class Calendar extends Component {
             { this.props.showArrows && this.state.stage === DAY_SELECTOR && nextMonthValid ?
               <TouchableHighlight
                 hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
-                style={{ padding: 4 }}
                 underlayColor={barStyle ? barStyle.backgroundColor : 'transparent'}
                 onPress={this._nextMonth}
               >
